db62f2d6915bd4569346e184f67e0525e1940627
Christopher Thorn <christopher.thorn@puppetlabs.com>
2021-02-12 09:29:01 -0800

    (PE-31028) Update dogfood script
    This PR updates the script to look up the value of the latest release
    candate build and compare that to the dogfood version.
    If they are the same we return. Otherwise we put up a PR to cjc to
    update to the latest version.

--- vars/bash/bump_dogfood_version.sh
+++ vars/bash/bump_dogfood_version.sh
@@ -1,20 +1,26 @@
-#USAGE: ./bump_dogfood_version.sh <version>
+#!/bin/sh
+## USAGE: ./bump_dogfood_version.sh
 
-readonly VERSION=$1 # PE build we're testing upgrades from in the dogfood job, examle: 2021.0.0-rc5-94-g6b3daba
-readonly TEMP_BRANCH="auto/master/bumping_dogfood_build_to_${VERSION}"
+rm -rf ./ci-job-configs
+git clone git@github.com:puppetlabs/ci-job-configs ./ci-job-configs
+cd ci-job-configs || exit
+
+readonly CJC_DOGFOOD_VERSION=$(sed -n "s/^    p_dogfooding_upgrade_from_rc_build: '\(.*\)'$/\1/gp" ./jenkii/enterprise/projects/pe-integration.yaml) # Example 2021.4.0-rc0
+readonly PE_FAMILY='2021.0'
+readonly LATEST_RELEASE_VERSION=$(curl -k https://artifactory.delivery.puppetlabs.net/artifactory/generic_enterprise__local/archives/internal/${PE_FAMILY}/LATEST)
+readonly TEMP_BRANCH="auto/master/bumping_dogfood_build_to_${LATEST_RELEASE_VERSION}"
+
+echo "CJC_DOGFOOD_VERSION=${CJC_DOGFOOD_VERSION}"
+echo "LATEST_RELEASE_VERSION=${LATEST_RELEASE_VERSION}"
 
 main() {
-  rm -rf ./ci-job-configs
-  git clone git@github.com:puppetlabs/ci-job-configs ./ci-job-configs
-  cd ci-job-configs
-
-  git checkout -b $TEMP_BRANCH
-  sed -i "/p_dogfooding_upgrade_from_rc_build:/c\    p_dogfooding_upgrade_from_rc_build: '${VERSION}'" ./jenkii/enterprise/projects/pe-integration.yaml
+  git checkout -b "$TEMP_BRANCH"
+  sed -i "/p_dogfooding_upgrade_from_rc_build:/c\    p_dogfooding_upgrade_from_rc_build: '${LATEST_RELEASE_VERSION}'" ./jenkii/enterprise/projects/pe-integration.yaml
   git add ./jenkii/enterprise/projects/pe-integration.yaml
-  git commit -m "Installer team bump for dogfooding upgrade build to ${VERSION}"
+  git commit -m "Installer team bump for dogfooding upgrade build to ${LATEST_RELEASE_VERSION}"
 
   echo 'Pushing changes to upstream...'
-  git push origin $TEMP_BRANCH
+  git push origin "$TEMP_BRANCH"
 
   echo 'Creating PR...'
   PULL_REQUEST="$(git show -s --pretty='format:%s%n%n%b' | hub pull-request -b master -F -)"
@@ -22,8 +28,14 @@ main() {
 
 error_exit() {
   local msg=$1
-  echo $msg
+  echo "$msg"
   exit 1
 }
-main || error_exit 'Release job creation failed'
-echo "Successfully Opened PR for $(pwd): ${PULL_REQUEST}"
+
+if [ "${CJC_DOGFOOD_VERSION}" = "${LATEST_RELEASE_VERSION}" ]; then
+  echo 'Dogfood version is up to date'
+  exit 0
+else
+  main || error_exit 'Release job creation failed'
+  echo "Successfully Opened PR for $(pwd): ${PULL_REQUEST}"
+fi
--- vars/bump_installers_dogfood_version.groovy
+++ vars/bump_installers_dogfood_version.groovy
@@ -1,10 +1,10 @@
-def call(String version) {
+def call(String empty_setting) {
   //Execute bash script, catch and print output and errors
   node('worker') {
     withCredentials([string(credentialsId: 'githubtoken', variable: 'GITHUB_TOKEN')]) {
       sh "curl -O https://raw.githubusercontent.com/puppetlabs/puppet_jenkins_shared_libraries/main/vars/bash/bump_dogfood_version.sh"
-      sh "chmod +x bump_dogfood_version.sh"
-      sh "bash bump_dogfood_version.sh $version"
+      sh 'chmod +x bump_dogfood_version.sh'
+      sh 'bash bump_dogfood_version.sh'
     }
   }
 }
