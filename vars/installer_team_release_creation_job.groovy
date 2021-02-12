c437f0725a508afd1155bfef2c3227b95f228a1a
shaigy <shaigy@puppet.com>
2021-02-12 10:51:17 -0800

    Add hour parameter
    Add hour parameter to the groovy script

--- vars/installer_team_release_creation_job.groovy
+++ vars/installer_team_release_creation_job.groovy
@@ -1,4 +1,4 @@
-def call(String version, String codename) {
+def call(String version, String codename, String hour) {
 
   if (version =~ '^20[0-9]{2}[.]([0-9]*)[.]([0-9]*)$') {
     println "${version} is a valid version"
@@ -6,12 +6,18 @@ def call(String version, String codename) {
     println "${version} is an invalid version"
     throw new Exception("Invalid version")
   }
+  if (hour =~ '^[0-9]?[0-9]$') {
+    println "${hour} is a valid time"
+  } else {
+    println "${hour} is an invalid time"
+    throw new Exception("Invalid hour")
+  }
   //Execute bash script, catch and print output and errors
   node('worker') {
     withCredentials([string(credentialsId: 'githubtoken', variable: 'GITHUB_TOKEN')]) {
       sh "curl -O https://raw.githubusercontent.com/puppetlabs/puppet_jenkins_shared_libraries/main/vars/bash/installer_team_release_creation_job.sh"
       sh "chmod +x installer_team_release_creation_job.sh"
-      sh "bash installer_team_release_creation_job.sh $version $codename"
+      sh "bash installer_team_release_creation_job.sh $version $codename $hour"
     }
   }
 }
