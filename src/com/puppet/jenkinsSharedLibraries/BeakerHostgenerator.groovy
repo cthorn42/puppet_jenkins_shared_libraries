6baf666f206fa999cdf9f1f4ee9b7a5aafce84d7
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2021-10-07 21:50:38 +0000

    (maint) Fix vmpooler usage
    This switches installer shim acceptance back to using --hypervisor vmpooler, but updates the pooling_api parameter to point to the new URL. This will tide us over until we can get ABS working for this repo.

--- src/com/puppet/jenkinsSharedLibraries/BeakerHostgenerator.groovy
+++ src/com/puppet/jenkinsSharedLibraries/BeakerHostgenerator.groovy
@@ -11,8 +11,9 @@ class BeakerHostgenerator extends RvmEnvironment {
                         String hypervisor,
                         String hostfile=null) {
         super(rubyVersion)
+        pooling_api = (hypervisor == 'vmpooler') ? ',pooling_api=https://vmpooler-prod.k8s.infracore.puppet.net/api/v1' : ''
         String hostgeneratorString = "bundle exec beaker-hostgenerator ${platform} --hypervisor ${hypervisor}\
- --global-config forge_host=forge-aio01-petest.puppetlabs.com --pe_dir ${peDir} --pe_ver ${peVersion} >&1 | tee ${hostfile}"
+ --global-config '{forge_host=forge-aio01-petest.puppetlabs.com${pooling_api}}' --pe_dir ${peDir} --pe_ver ${peVersion} >&1 | tee ${hostfile}"
         this.hostgeneratorScript = this.rvmCommand + """
 export BUNDLE_BIN=.bundle/bin
 export BUNDLE_PATH=.bundle/gems
